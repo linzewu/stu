386a4ed66762ed999d8984f021c2d97fbad1b3cb
admin <595157992@qq.com>
2019-05-27 14:17:06 +0800

    update

--- base-module/src/main/java/com/xs/jt/base/module/aop/SpecialTimeslotAdvice.java
+++ base-module/src/main/java/com/xs/jt/base/module/aop/SpecialTimeslotAdvice.java
@@ -51,50 +51,51 @@ public class SpecialTimeslotAdvice {
 	@Around("controllerAspect()")
 	public Object around(ProceedingJoinPoint pjp) throws Throwable {
 		SecurityAuditPolicySetting saps = sapsImpl.getPolicyByCode(SecurityAuditPolicySetting.SPECIAL_TIMESLOT_VISIT);
-		String timeClz =saps.getClz();
-		if(!StringUtils.isEmpty(timeClz)) {
-			String[] times =  timeClz.split(",");
-			
-			SimpleDateFormat sdf =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			
-			Date currentDate=Calendar.getInstance().getTime();
-			
-			for(String time:times) {
-				String[] timeStr =time.split("-");
-				String startTime =timeStr[0];
-				String endTime =timeStr[1];
-				String[] startHHMM = startTime.split(":");
-				String startHH=startHHMM[0];
-				String startMM=startHHMM[1];
-				Calendar startCalendar = Calendar.getInstance();
-				startCalendar.set(Calendar.HOUR_OF_DAY, Integer.valueOf(startHH));
-				startCalendar.set(Calendar.MINUTE, Integer.valueOf(startMM));
-				String[] endHHMM = endTime.split(":");
-				String endHH=endHHMM[0];
-				String endMM=endHHMM[1];
+		if(saps != null) {
+			String timeClz =saps.getClz();
+			if(!StringUtils.isEmpty(timeClz)) {
+				String[] times =  timeClz.split(",");
 				
-				Calendar endCalendar = Calendar.getInstance();
-				endCalendar.set(Calendar.HOUR_OF_DAY, Integer.valueOf(endHH));
-				endCalendar.set(Calendar.MINUTE, Integer.valueOf(endMM));
-				if(currentDate.getTime()>startCalendar.getTimeInMillis()&&currentDate.getTime()<endCalendar.getTimeInMillis()) {
+				SimpleDateFormat sdf =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				
+				Date currentDate=Calendar.getInstance().getTime();
+				
+				for(String time:times) {
+					String[] timeStr =time.split("-");
+					String startTime =timeStr[0];
+					String endTime =timeStr[1];
+					String[] startHHMM = startTime.split(":");
+					String startHH=startHHMM[0];
+					String startMM=startHHMM[1];
+					Calendar startCalendar = Calendar.getInstance();
+					startCalendar.set(Calendar.HOUR_OF_DAY, Integer.valueOf(startHH));
+					startCalendar.set(Calendar.MINUTE, Integer.valueOf(startMM));
+					String[] endHHMM = endTime.split(":");
+					String endHH=endHHMM[0];
+					String endMM=endHHMM[1];
 					
-					SecurityLog securityLog = new SecurityLog();
-					securityLog.setCreateUser(User.SYSTEM_USER);
-					securityLog.setUpdateUser(User.SYSTEM_USER);
-					securityLog.setIpAddr(Common.getIpAdrress(request));
-					securityLog.setClbmmc("规定时段外访");
-					securityLog.setClbm(SecurityAuditPolicySetting.SPECIAL_TIMESLOT_VISIT);
-					securityLog.setSignRed("N");
-					securityLog.setContent("规定时段外访问系统，拒绝访问。");
-					securityLog.setResult("拒绝访问");
-					securityLogManager.saveSecurityLog(securityLog);
-					session.invalidate();
-					throw new SpecialTimeslotException("规定时段外访问系统，拒绝访问。");
+					Calendar endCalendar = Calendar.getInstance();
+					endCalendar.set(Calendar.HOUR_OF_DAY, Integer.valueOf(endHH));
+					endCalendar.set(Calendar.MINUTE, Integer.valueOf(endMM));
+					if(currentDate.getTime()>startCalendar.getTimeInMillis()&&currentDate.getTime()<endCalendar.getTimeInMillis()) {
+						
+						SecurityLog securityLog = new SecurityLog();
+						securityLog.setCreateUser(User.SYSTEM_USER);
+						securityLog.setUpdateUser(User.SYSTEM_USER);
+						securityLog.setIpAddr(Common.getIpAdrress(request));
+						securityLog.setClbmmc("规定时段外访");
+						securityLog.setClbm(SecurityAuditPolicySetting.SPECIAL_TIMESLOT_VISIT);
+						securityLog.setSignRed("N");
+						securityLog.setContent("规定时段外访问系统，拒绝访问。");
+						securityLog.setResult("拒绝访问");
+						securityLogManager.saveSecurityLog(securityLog);
+						session.invalidate();
+						throw new SpecialTimeslotException("规定时段外访问系统，拒绝访问。");
+					}
+					
 				}
-				
 			}
 		}
-	
 		 
 		 
 		
